a85bee540f749f0de314c603442fa5bb0da15b5e
halite-inc <halite.dev@gmail.com>
2026-07-10 19:51:41 +0530

    Initial Commit

---
+++ appleint/appleintApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleintApp.swift
+//  appleint
+//
+//  Created by Vijay on 10/07/26.
+//
+
+import SwiftUI
+
+@main
+struct appleintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
